eea176e002af881f22f127cc5926d27b1f6c1702
jjongwon7 <jjongwon7@naver.com>
2024-01-05 23:51:57 +0900

    feat: Add Spring Security Config

---
+++ src/main/java/com/and20roid/backend/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.and20roid.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors((AbstractHttpConfigurer::disable))
+                .formLogin((AbstractHttpConfigurer::disable));
+
+        httpSecurity
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/users/signup", "/users/login").permitAll()
+                        .anyRequest().authenticated());
+
+        return httpSecurity.build();
+    }
+}
