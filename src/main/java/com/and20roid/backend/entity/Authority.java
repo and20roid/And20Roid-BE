e0b6f5928967e68d247189da1e6e1573863eb753
jjongwon7 <jjongwon7@naver.com>
2024-01-06 18:05:35 +0900

    feat: ADD entity
    - Authority.java

---
+++ src/main/java/com/and20roid/backend/entity/Authority.java
@@ -0,0 +1,31 @@
+package com.and20roid.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String authority;
+
+    public Authority(User user, String authority) {
+        this.user = user;
+        this.authority = authority;
+    }
+}
---
+++ src/main/java/com/and20roid/backend/repository/AuthorityRepository.java
@@ -0,0 +1,7 @@
+package com.and20roid.backend.repository;
+
+import com.and20roid.backend.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+}
