ef9d5e30ba4b213d7fdc9bd5ab8e82dc1071daae
jjongwon7 <jjongwon7@naver.com>
2024-01-10 23:07:08 +0900

    feat: s3 objectMetadata 수정
    - add setContentLength

--- src/main/java/com/and20roid/backend/service/AwsS3Service.java
+++ src/main/java/com/and20roid/backend/service/AwsS3Service.java
@@ -36,6 +36,7 @@ public class AwsS3Service {
 
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(multipartFile.getContentType());
+        objectMetadata.setContentLength(multipartFile.getSize());
 
         try(InputStream inputStream = multipartFile.getInputStream()) {
             amazonS3.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
